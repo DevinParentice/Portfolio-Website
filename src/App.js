aa2d0347df883a525e0c80316ce7c4c5303d1db3
DevinParentice <dude114100@gmail.com>
2021-02-19 17:32:44 -0500

    Added animated background

--- src/App.js
+++ src/App.js
@@ -1,17 +1,47 @@
 import React from "react";
+import CELLS from 'vanta/dist/vanta.cells.min';
 import Project1 from "./components/Project1";
 import SplashPage from './components/SplashPage'
 import "./styles/App.css";
 
-function App() {
-  return (
-    <div className="App bg-gray-900 overflow-hidden">
-			<div className="sections">
-				<SplashPage />
-				<Project1 />
+class App extends React.Component {
+
+	constructor() {
+		super();
+		this.vantaRef = React.createRef();
+	}
+
+	componentDidMount() {
+		this.vantaEffect = CELLS({
+			el: this.vantaRef.current,
+			mouseControls: true,
+			touchControls: true,
+			gyroControls: false,
+			minHeight: 200.00,
+			minWidth: 200.00,
+			scale: 1.00,
+			color1: 0x343434,
+			color2: 0x0,
+			size: 2.50,
+			speed: 0.00
+		});
+	}
+
+	componentWillUnmount() {
+		if (this.vantaEffect) this.vantaEffect.destroy();
+	}
+
+	render() {
+		return (
+			<div className="App bg-gray-900 overflow-hidden">
+				<div ref={this.vantaRef} className="sections">
+					<SplashPage />
+					<Project1 />
+				</div>
 			</div>
-		</div>
-  );
+		  );
+	}
+
 }
 
 export default App;
