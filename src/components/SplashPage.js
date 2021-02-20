7cb40d46e7dbd650a580f1bb8cef435448e75fb0
DevinParentice <dude114100@gmail.com>
2021-02-20 15:04:01 -0500

    Fixed padding issue

--- src/components/SplashPage.js
+++ src/components/SplashPage.js
@@ -17,9 +17,11 @@ class SplashPage extends React.Component {
 			<div ref={el => this.scene = el} className="flex flex-row w-screen h-screen items-center">
 				<div className="container" data-depth="0.20">
 					<h1 className="main-text text-6xl sm:text-9xl text-white ml-10">Devin Parentice</h1>
-					<a href={pdf} without rel="noopener noreferrer" target="_blank">
-						<h3 className="sub-text text-2xl sm:text-4xl text-white ml-10 border-b-2 hover:text-gray-400 hover:border-gray-400">View Resume</h3>
-					</a>
+					<div className="flex">
+						<a href={pdf} without rel="noopener noreferrer" target="_blank">
+							<h3 className="sub-text text-2xl sm:text-4xl text-white ml-10 border-b-2 hover:text-gray-400 hover:border-gray-400">View Resume</h3>
+						</a>
+					</div>
 				</div>
 			</div>
 		);
