709021b5cab715710b4fed5a9740d808bd9a1fa2
DevinParentice <dude114100@gmail.com>
2021-02-20 16:47:51 -0500

    Added picture to first project

--- src/components/Project1.js
+++ src/components/Project1.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Parallax from 'parallax-js';
+import P1Images from './P1Images';
 class Project1 extends React.Component {
 	
 	componentDidMount() {
@@ -13,7 +14,7 @@ class Project1 extends React.Component {
 	render() {
 		return (
 			<section ref={el => this.scene = el} className="flex w-screen h-screen items-center">
-				<div className="container" data-depth="0.20">
+				<div className="container z-10" data-depth="0.20">
 					<h1 className="main-text text-6xl sm:text-9xl text-white ml-8">Cirillo's Artisan Pizza</h1>
 					<div className="flex">
 						<a href="https://www.cirilloswestchester.com/" target="_blank">
@@ -23,6 +24,9 @@ class Project1 extends React.Component {
 							<h3 className="sub-text text-2xl sm:text-4xl text-white ml-8 border-b-2 hover:text-gray-400 hover:border-gray-400">View Code</h3>
 						</a>
 					</div>
+				</div>
+				<div className="container" data-depth="0.40">
+					<P1Images />
 				</div>
 			</section>
 		);
